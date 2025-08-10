5eda2f82bb465ac067d83bef4e9af12ed4cf2308
Sankar <gsankarasubramaniann@gmail.com>
2025-08-10 10:20:37 +0530

    Remove unused React import to fix Vercel build

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Textarea } from './ui/textarea'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
